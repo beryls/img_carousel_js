b814a0bfa640c506d6b8f70c94fc196be9620fbb
Beryl <beryl.schragger@gmail.com>
2013-07-24 19:31:22 -0400

    Add opacity changes for previous and next buttons (both mouseover and mouseout) and add functions to change picture on click of buttons.

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,50 @@
-var carousel = document.getElementById('carousel');
+window.onload = function() {
 
-// Buttons
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
+  var carousel = document.getElementById('carousel');
 
-carousel.style.marginLeft = 0;
+  // Buttons
+  var next = document.getElementById('next');
+  var previous = document.getElementById('previous');
 
-// Create mouseoveer and mouseexit events for the buttons
-// to change their opacity from .5 to .2
+  carousel.style.marginLeft = 0;
+  var leftAmount = 0;
 
-// Create onclick events to advance the images
+  var toRight = function() {
+    leftAmount -= 612;
+    carousel.style.marginLeft = (-1 * Math.abs(leftAmount%1836)) + "px";
+  }
+
+  var toLeft = function() {
+    leftAmount -= 1224;
+    carousel.style.marginLeft = (-1 * Math.abs(leftAmount%1836)) + "px";
+  }
+
+  next.onclick = function(event) {
+    toRight();
+  }
+
+  previous.onclick = function(event) {
+    toLeft();
+  }
+
+  next.onmouseover = function(event) {
+    this.style.opacity = 0.2;
+  }
+
+  next.onmouseout = function(event) {
+    this.style.opacity = 0.5;
+  }
+
+  previous.onmouseover = function(event) {
+    this.style.opacity = 0.2;
+  }
+
+  previous.onmouseout = function(event) {
+    this.style.opacity = 0.5;
+  }
+  // Create mouseoveer and mouseexit events for the buttons
+  // to change their opacity from .5 to .2
+
+  // Create onclick events to advance the images
+
+}
