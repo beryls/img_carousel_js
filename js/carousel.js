a01349bda6b5aa0c31d56b884c66f41d9b729212
Beryl <beryl.schragger@gmail.com>
2013-07-24 19:50:35 -0400

    Reorganize functions, simplify picture change functions, and add slide show button.

--- js/carousel.js
+++ js/carousel.js
@@ -11,21 +11,18 @@ window.onload = function() {
 
   var toRight = function() {
     leftAmount -= 612;
-    carousel.style.marginLeft = (-1 * Math.abs(leftAmount%1836)) + "px";
+    leftAmount = leftAmount%1836;
+    carousel.style.marginLeft = leftAmount + "px";
   }
 
   var toLeft = function() {
     leftAmount -= 1224;
-    carousel.style.marginLeft = (-1 * Math.abs(leftAmount%1836)) + "px";
+    leftAmount = leftAmount%1836;
+    carousel.style.marginLeft = leftAmount + "px";
   }
 
-  next.onclick = function(event) {
-    toRight();
-  }
-
-  previous.onclick = function(event) {
-    toLeft();
-  }
+  // Create mouseoveer and mouseexit events for the buttons
+  // to change their opacity from .5 to .2
 
   next.onmouseover = function(event) {
     this.style.opacity = 0.2;
@@ -42,9 +39,16 @@ window.onload = function() {
   previous.onmouseout = function(event) {
     this.style.opacity = 0.5;
   }
-  // Create mouseoveer and mouseexit events for the buttons
-  // to change their opacity from .5 to .2
 
   // Create onclick events to advance the images
+
+
+  next.onclick = function(event) {
+    toRight();
+  }
+
+  previous.onclick = function(event) {
+    toLeft();
+  }
 
 }
